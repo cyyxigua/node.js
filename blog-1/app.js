0a0684884b574ae0c80ba6f8e1b2df7bbf5f54be
yychen <yychen@utstar.com>
2020-05-27 15:50:17 +0800

    session nginx反向代理

--- blog-1/app.js
+++ blog-1/app.js
@@ -2,6 +2,16 @@ const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
+// session 数据
+const SESSION_DATA = {}
+
+// 获取 cookie 的过期时间 
+const getCookieExpires = () => {
+  const d = new Date()
+  d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+  return d.toGMTString()
+}
+
 // 用于处理 post data
 const getPostData = (req) => {
   const promise = new Promise((resolve, reject) => {
@@ -54,6 +64,20 @@ const serverHandle = (req, res) => {
     req.cookie[key] = val
   });
 
+  // 解析 session
+  let needSetCookie = false
+  let userId = req.cookie.userid
+  if(userId) {
+    if(!SESSION_DATA[userId]) {
+      SESSION_DATA[userId] = {}
+    }
+  } else {
+    needSetCookie = true
+    userId = `${Date.now()}_${Math.random()}`
+    SESSION_DATA[userId] = {}
+  }
+  req.session = SESSION_DATA[userId]
+ 
   // 处理post data
   getPostData(req).then(postData => {
     req.body = postData
@@ -69,6 +93,11 @@ const serverHandle = (req, res) => {
     const blogResult = handleBlogRouter(req, res)
     if(blogResult) {
       blogResult.then(blogData => {
+        if(needSetCookie) {
+          // 操作 cookie( httpOnly 保证 cookie 不能被前端修改，expires 设置 cookie 过期时间)
+          res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+        }
+
         res.end(
           JSON.stringify(blogData)
         )
@@ -88,6 +117,11 @@ const serverHandle = (req, res) => {
     const userResult = handleUserRouter(req, res)
     if(userResult) {
       userResult.then(userData => {
+        if(needSetCookie) {
+          // 操作 cookie( httpOnly 保证 cookie 不能被前端修改，expires 设置 cookie 过期时间)
+          res.setHeader('Set-Cookie', `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
+        }
+
         res.end(
           JSON.stringify(userData)
         )
--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -1,26 +1,21 @@
 const { login } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
-// 获取 cookie 的过期时间 
-const getCookieExpires = () => {
-  const d = new Date()
-  d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
-  return d.toGMTString()
-}
-
 const handleUserRouter = (req, res) => {
   const method = req.method
 
   // 登录
-  if(method === 'GET' && req.path === '/api/user/login') {
-    // const { username, password } = req.body
-    const { username, password } = req.query
+  if(method === 'POST' && req.path === '/api/user/login') {
+    const { username, password } = req.body
+    // const { username, password } = req.query
     const result = login(username, password)
     return result.then(data => {
       if(data.username) {
+        // 设置 session
+        req.session.username = data.username
+        req.session.realname = data.realname
 
-        // 操作 cookie( httpOnly 保证 cookie 不能被前端修改，expires 设置 cookie 过期时间)
-        res.setHeader('Set-Cookie', `username=${data.username}; path=/; httpOnly; expires=${getCookieExpires()}`)
+        console.log('req.session is', req.session)
 
         return new SuccessModel()
       }
@@ -29,14 +24,14 @@ const handleUserRouter = (req, res) => {
   }
 
   // 登录验证测试
-  if(method === 'GET' && req.path === '/api/user/login-test') {
-    if(req.cookie.username) {
-      return Promise.resolve(new SuccessModel({
-        username: req.cookie.username
-      }))
-    }
-    return Promise.resolve(new ErrorModel('尚未登录'))
-  }
+  // if(method === 'GET' && req.path === '/api/user/login-test') {
+  //   if(req.session.username) {
+  //     return Promise.resolve(new SuccessModel({
+  //       session: req.session
+  //     }))
+  //   }
+  //   return Promise.resolve(new ErrorModel('尚未登录'))
+  // }
 }
 
 module.exports = handleUserRouter
